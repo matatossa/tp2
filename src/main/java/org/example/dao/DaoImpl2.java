e4b005b74895a6b4766a1bfcefdedd29e72d87c4
El mehdi <dertanlee@gmail.com>
2025-10-14 21:44:22 +0100

    Initial commit of TP2 project

---
+++ src/main/java/org/example/dao/DaoImpl2.java
@@ -0,0 +1,13 @@
+package org.example.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Profile;
+@Component("dao2")
+@Profile("dev")  // Ce bean sera actif uniquement avec le profil "dev"
+public class DaoImpl2 implements IDao {
+    @Override
+    public double getValue() {
+        System.out.println("Version de développement");
+        return 150.0;
+    }
+}
---
+++ src/main/java/org/example/metier/IMetier.java
@@ -0,0 +1,6 @@
+package org.example.metier;
+
+
+public interface IMetier {
+    double calcul();
+}
